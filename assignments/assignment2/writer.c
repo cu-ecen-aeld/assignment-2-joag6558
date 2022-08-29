e970010bcf431ce8043c5d338635c90e7dec41c0
joag6558 <joag6558@github.com>
2022-08-29 14:48:46 -0600

    writer.c fix

--- assignments/assignment2/writer.c
+++ assignments/assignment2/writer.c
@@ -1,31 +1,41 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <syslog.h>
+
 
 
 int main(int argc, char *argv[])
 {
 int fd;
-  
-	if ((fd = open(argv[1], "r")) == NULL)
+
+ openlog("COURSE 1 ASSIGNMENT 2", LOG_NDELAY, LOG_DAEMON);
+ syslog(LOG_DEBUG, "Writing %s to file %s", argv[2], argv[1]);
+
+ /*printf( "argc %d\n", argc);*/
+ 
+ if(argc < 3){
+	syslog(LOG_ERR, "Usage: writer.sh /tmp/aesd/assignment1/sample.txt ios \n");
+        return 1;
+    }
+     
+ if ((fd = open(argv[1], O_RDWR|O_CREAT, 0644)) == NULL)
 	{
-	     /* First string (%s) is program name (argv[0]).  */
-	     /* Second string (%s) is name of file that could */
-	     /* not be opened (argv[1]). */
+	     /* argv[0] program name.  */
+	     /* argv[1] first argument */
 
-
-
-	     (void)fprintf(stderr, "%s: Cannot open input file %s\n",
-		   argv[0], argv[1]);
-	     return(2);
+	     syslog(LOG_ERR, "Cannot open input file %s\n",
+		   argv[1]);
+	     return(1);
 	}
-	else
-	{
-	     printf("%s: Cannot open input file %s\n",
-		   argv[0], argv[1]);
-	}
+
+ write(fd,argv[2],sizeof(argv[2])+3);
+ write(fd,"\n",1);
+
+ close(fd);
 	
                
-  return EXIT_SUCCESS;
+ /*return 1;*/
 }
 
--- finder-app/writer.c
+++ finder-app/writer.c
@@ -13,10 +13,11 @@ int fd;
  openlog("COURSE 1 ASSIGNMENT 2", LOG_NDELAY, LOG_DAEMON);
  syslog(LOG_DEBUG, "Writing %s to file %s", argv[2], argv[1]);
 
+ /*printf( "argc %d\n", argc);*/
  
- if(argc < 2){
+ if(argc < 3){
 	syslog(LOG_ERR, "Usage: writer.sh /tmp/aesd/assignment1/sample.txt ios \n");
-        return -1;
+        return 1;
     }
      
  if ((fd = open(argv[1], O_RDWR|O_CREAT, 0644)) == NULL)
@@ -26,7 +27,7 @@ int fd;
 
 	     syslog(LOG_ERR, "Cannot open input file %s\n",
 		   argv[1]);
-	     return(2);
+	     return(1);
 	}
 
  write(fd,argv[2],sizeof(argv[2])+3);
@@ -35,6 +36,6 @@ int fd;
  close(fd);
 	
                
-  return EXIT_SUCCESS;
+ /*return 1;*/
 }
 
